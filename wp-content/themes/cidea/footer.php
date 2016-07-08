d8bb7dc285beb1fbe90fd9094e608a96dd7c2372
moosenado <egrabish@gmail.com>
2016-07-07 20:00:54 -0400

    footer styling

--- wp-content/themes/cidea/footer.php
+++ wp-content/themes/cidea/footer.php
@@ -2,6 +2,27 @@
 	<div class="footer-container" style="background-image: url(<?php echo home_url(); ?>/wp-content/uploads/2016/07/cidea-logo-swirl-svg.png)">
 		<div class="footer-bg-overlay">
 		</div>
+		<div class="centered-container">
+			<div class="centered">
+				<object data="<?php echo home_url(); ?>/wp-content/uploads/2016/07/cidea-logo-svg-transparent-final.svg" type="image/svg+xml">
+        			<img src="<?php echo home_url(); ?>/wp-content/uploads/2016/07/cidea-logo-svg-transparent-final.png" />
+        		</object>
+        		<div class="address-container">
+        			<ul>
+        				<li><?php echo get_option('address'); ?></li>
+        				<li><?php echo get_option('city'); ?></li>
+        				<li><?php echo get_option('postalcode'); ?></li>
+        			</ul>
+        		</div>
+        		<div class="contact-container">
+        			<ul>
+        				<li><?php echo get_option('phonenumber'); ?></li>
+        				<li><?php echo get_option('email'); ?></li>
+        			</ul>
+        		</div>
+
+			</div>
+		</div>	
 	</div>
 
 </div>
--- wp-content/themes/cidea/functions.php
+++ wp-content/themes/cidea/functions.php
@@ -9,3 +9,46 @@ if (!function_exists('cc_mime_types'))
 	}
 	add_filter('upload_mimes', 'cc_mime_types');
 }
+
+if (!function_exists('add_global_custom_options'))
+{
+
+
+function add_global_custom_options()
+{
+    add_options_page('Footer Options', 'Footer Options', 'manage_options', 'functions','global_custom_options');
+}
+
+add_action('admin_menu', 'add_global_custom_options');
+
+function global_custom_options()
+{
+?>
+    <div class="wrap">
+        <h2>Footer Options</h2>
+        <form method="post" action="options.php">
+            <?php wp_nonce_field('update-options') ?>
+            <p><strong>Address:</strong><br />
+                <input type="text" name="address" size="45" value="<?php echo get_option('address'); ?>" />
+            </p>
+            <p><strong>City:</strong><br />
+                <input type="text" name="city" size="45" value="<?php echo get_option('city'); ?>" />
+            </p>
+            <p><strong>Postal Code:</strong><br />
+                <input type="text" name="postalcode" size="45" value="<?php echo get_option('postalcode'); ?>" />
+            </p>
+            <p><strong>Phone Number:</strong><br />
+                <input type="text" name="phonenumber" size="45" value="<?php echo get_option('phonenumber'); ?>" />
+            </p>
+            <p><strong>Email:</strong><br />
+                <input type="text" name="email" size="45" value="<?php echo get_option('email'); ?>" />
+            </p>
+            <p><input type="submit" name="Submit" value="Save" /></p>
+            <input type="hidden" name="action" value="update" />
+            <input type="hidden" name="page_options" value="address,city,postalcode,phonenumber,email" />
+        </form>
+    </div>
+<?php
+}
+}
+?>
