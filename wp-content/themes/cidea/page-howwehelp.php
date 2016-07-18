35fbb7c9373601ddebd866ebaeab6fbd58c21e10
moosenado <egrabish@gmail.com>
2016-07-18 08:00:16 -0400

    business case page grid

--- wp-content/themes/cidea/page-howwehelp.php
+++ wp-content/themes/cidea/page-howwehelp.php
@@ -40,5 +40,61 @@
 	</div>
 </div>
 
+<div class="second-section-container">
+	<h2><?php echo get_field('second_section_title'); ?></h2>
+
+	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
+		<div class="business-case-cont">
+			<div class="centered-container">
+				<div class="centered">
+					<div class="content-container">
+						<h3><?php echo get_field('business_case_section_one_title'); ?></h3>
+						<div><?php echo get_field('business_case_section_one_paragraph'); ?></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
+		<div class="business-case-cont bg" style="background-image: url(<?php echo get_field('business_case_section_one_image')['url']; ?>)"></div>
+	</div>
+
+	<div class="col-xs-12 col-sm-12 col-md-push-6 col-md-6 col-lg-push-6 col-lg-6 col-xlg-push-6 col-xlg-6 remove-pad-marg">
+		<div class="business-case-cont">
+			<div class="centered-container">
+				<div class="centered">
+					<div class="content-container">
+						<h3><?php echo get_field('business_case_section_two_title'); ?></h3>
+						<div><?php echo get_field('business_case_section_two_paragraph'); ?></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-xs-12 col-sm-12 col-md-pull-6 col-md-6 col-lg-pull-6 col-lg-6 col-xlg-pull-6 col-xlg-6 remove-pad-marg">
+		<div class="business-case-cont bg" style="background-image: url(<?php echo get_field('business_case_section_two_image')['url']; ?>)"></div>
+	</div>
+
+	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
+		<div class="business-case-cont">
+			<div class="centered-container">
+				<div class="centered">
+					<div class="content-container">
+						<h3><?php echo get_field('business_case_section_three_title'); ?></h3>
+						<div><?php echo get_field('business_case_section_third_paragraph'); ?></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
+		<div class="business-case-cont bg" style="background-image: url(<?php echo get_field('business_case_section_three_image')['url']; ?>)"></div>
+	</div>
+
+</div>
+
 
 <?php get_footer(); ?>
