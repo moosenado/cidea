551a5c94da10e61805023fbfea8afc8f38dac2c7
moosenado <egrabish@gmail.com>
2016-07-18 20:41:45 -0400

    mobile styling

--- wp-content/themes/cidea/page-howwehelp.php
+++ wp-content/themes/cidea/page-howwehelp.php
@@ -57,7 +57,7 @@
 	</div>
 	
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
-		<div class="business-case-cont bg" style="background-image: url(<?php echo get_field('business_case_section_one_image')['url']; ?>)"></div>
+		<div class="business-case-cont-img bg" style="background-image: url(<?php echo get_field('business_case_section_one_image')['url']; ?>)"></div>
 	</div>
 
 	<div class="col-xs-12 col-sm-12 col-md-push-6 col-md-6 col-lg-push-6 col-lg-6 col-xlg-push-6 col-xlg-6 remove-pad-marg">
@@ -74,7 +74,7 @@
 	</div>
 
 	<div class="col-xs-12 col-sm-12 col-md-pull-6 col-md-6 col-lg-pull-6 col-lg-6 col-xlg-pull-6 col-xlg-6 remove-pad-marg">
-		<div class="business-case-cont bg" style="background-image: url(<?php echo get_field('business_case_section_two_image')['url']; ?>)"></div>
+		<div class="business-case-cont-img bg" style="background-image: url(<?php echo get_field('business_case_section_two_image')['url']; ?>)"></div>
 	</div>
 
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
@@ -91,14 +91,18 @@
 	</div>
 
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
-		<div class="business-case-cont bg" style="background-image: url(<?php echo get_field('business_case_section_three_image')['url']; ?>)"></div>
+		<div class="business-case-cont-img bg" style="background-image: url(<?php echo get_field('business_case_section_three_image')['url']; ?>)"></div>
 	</div>
+
+	<div class="clearfix"></div>
 
 </div>
 
-<div class="bottom-section-container">
+<div class="clearfix"></div>
+
+<div class="third-section-container">
 	<h3><?php echo get_field('third_section_title'); ?></h3>
-	<div class="bottom-section-content"><?php echo get_field('third_section_paragraph'); ?></div>
+	<div class="third-section-content"><?php echo get_field('third_section_paragraph'); ?></div>
 </div>
 
 
