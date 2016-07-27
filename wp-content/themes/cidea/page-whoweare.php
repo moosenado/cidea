3478c98c1ad817b1c4feb7440e241f9c39337607
moosenado <egrabish@gmail.com>
2016-07-27 18:45:51 -0400

    no opacity add

--- wp-content/themes/cidea/page-whoweare.php
+++ wp-content/themes/cidea/page-whoweare.php
@@ -5,7 +5,7 @@
 <div class="wwa-top-page-banner fadeInSlower" style="background-image: url(<?php echo get_field('page_banner')['url']; ?>);">
 	<div class="centered-container">
 		<div class="centered">
-			<h1 class="hidden-xs scrollTime" data-animation="slideUpAnimation"><?php echo get_field('page_title'); ?></h1>
+			<h1 class="no-opacity hidden-xs scrollTime" data-animation="slideUpAnimation"><?php echo get_field('page_title'); ?></h1>
 			<div class="no-opacity scrollTime" data-animation="rotateInAnimation" data-timeout="500">
 				<object class="hidden-xs" data="<?php echo home_url(); ?>/wp-content/uploads/2016/07/svg-logo-swirl-transparent.svg" type="image/svg+xml">
 					<img src="<?php echo home_url(); ?>/wp-content/uploads/2016/07/cidea-logo-swirl-svg.png" class="hidden-xs" />
