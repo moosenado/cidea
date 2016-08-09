305ce8a82569a32311eef61121f7369655ece67d
moosenado <egrabish@gmail.com>
2016-08-09 07:54:51 -0400

    style cleanup

--- wp-content/themes/cidea/page-home.php
+++ wp-content/themes/cidea/page-home.php
@@ -202,14 +202,14 @@
 	<div class="bottom-section-container" style="background-image: url(<?php echo get_field('bottom_section_image')['url']; ?>);">
 		<div class="centered-container">
 			<div class="centered">
-				<div class="bottom-section-title scrollTime" data-animation="slideUpAnimation" data-timeout="1000">
+				<div class="bottom-section-title scrollTime" data-animation="slideUpAnimation" data-timeout="500">
 					<?php echo get_field('bottom_section_title'); ?>
 				</div>
-				<object data="<?php echo home_url(); ?>/wp-content/uploads/2016/07/svg-logo-swirl-transparent.svg" type="image/svg+xml" class="scrollTime" data-animation="rotateInAnimation" data-timeout="1500">
+				<object data="<?php echo home_url(); ?>/wp-content/uploads/2016/07/svg-logo-swirl-transparent.svg" type="image/svg+xml" class="scrollTime" data-animation="rotateInAnimation" data-timeout="1000">
 					<img src="<?php echo home_url(); ?>/wp-content/uploads/2016/07/cidea-logo-swirl-svg.png" class="scrollTime" data-animation="rotateInAnimation" data-timeout="1500"/>
 				</object>
 				<a href="<?php echo home_url(); ?>/who-we-are">
-					<div class="cidea-button scrollTime" data-animation="slideUpAnimation" data-timeout="1000">
+					<div class="cidea-button scrollTime" data-animation="slideUpAnimation" data-timeout="500">
 						<div class="centered-container">
 							<div class="centered">
 								Who We Are
