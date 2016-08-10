b95b0c24f4ea0ecd4efac289ab35a483727e9f4c
moosenado <egrabish@gmail.com>
2016-08-10 07:36:21 -0400

    final revisions

--- wp-content/themes/cidea/page-howwehelp.php
+++ wp-content/themes/cidea/page-howwehelp.php
@@ -64,7 +64,7 @@
 	</div>
 	
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
-		<div class="business-case-cont-img bg scrollTime" data-animation="fadeInSlow" data-timeout="600" style="background-image: url(<?php echo get_field('business_case_section_one_image')['url']; ?>)"></div>
+		<div class="business-case-cont-img bg scrollTime" data-animation="fadeInSlowWFilter" data-timeout="600" style="background-image: url(<?php echo get_field('business_case_section_one_image')['url']; ?>)"></div>
 	</div>
 
 	<div id="hwh-section2" class="col-xs-12 col-sm-12 col-md-push-6 col-md-6 col-lg-push-6 col-lg-6 col-xlg-push-6 col-xlg-6 remove-pad-marg">
@@ -81,7 +81,7 @@
 	</div>
 
 	<div class="col-xs-12 col-sm-12 col-md-pull-6 col-md-6 col-lg-pull-6 col-lg-6 col-xlg-pull-6 col-xlg-6 remove-pad-marg">
-		<div class="business-case-cont-img bg scrollTime" data-animation="fadeInSlow" data-timeout="600" style="background-image: url(<?php echo get_field('business_case_section_two_image')['url']; ?>)"></div>
+		<div class="business-case-cont-img bg scrollTime" data-animation="fadeInSlowWFilter" data-timeout="600" style="background-image: url(<?php echo get_field('business_case_section_two_image')['url']; ?>)"></div>
 	</div>
 
 	<div id="hwh-section3" class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
@@ -98,7 +98,7 @@
 	</div>
 
 	<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xlg-6 remove-pad-marg">
-		<div class="business-case-cont-img bg scrollTime" data-animation="fadeInSlow" data-timeout="600" style="background-image: url(<?php echo get_field('business_case_section_three_image')['url']; ?>)"></div>
+		<div class="business-case-cont-img bg scrollTime" data-animation="fadeInSlowWFilter" data-timeout="600" style="background-image: url(<?php echo get_field('business_case_section_three_image')['url']; ?>)"></div>
 	</div>
 
 	<div class="clearfix"></div>
